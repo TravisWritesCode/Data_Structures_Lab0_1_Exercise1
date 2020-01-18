a77c47d59815372ce682ca627f845e5cf30be6de
TravisWritesCode <37280847+TravisWritesCode@users.noreply.github.com>
2020-01-18 11:37:06 -0500

    lab0_1_exercise1

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << "Name: travis hescox" << endl;	
+	cout << "Phones: 813-727-2079" << endl;
+	cout << "email: thescox@students.kennesaw.edu" << endl;
+	cout << "Hometown: Tampa, FL" << endl;
+	cout << "High School(s): Joe E. Newsome High School" << endl;
+	cout << "Previous colleges: Santa Fe, Hillsborough Community, Chattahoochee Tech." << endl;
+	cout << "List college math/CS courses: Calc 1, Calc 2, Lin. Algebra, Discrete Math, Prog. 1, Prog. 2, Intro to Software Engineering, Database Systems" << endl;
+	cout << "What, when, and where was your last math and cs course? Last semester (Fall 2019) at KSU." << endl;
+	cout << "Type(s) of computers that you are confident working on: Windows and some Linux" << endl;
+	cout << "Extracurricular activities (jobs, clubs, sports, etc.): Hiking, gaming, working out, programming, learning new things." << endl;
+	cout << "Favorite books, movies, music: Typically, I like to read instructional books, I like gangster movies, and I mostly listen to indie rock and hip hop." << endl;
+	cout << "What you plan to do after graduation? I am still deciding on what I would like to do after graduation, but my goal at the moment is to move downtown in a big city and work as an automation engineer." << endl; 
+	return 0;
+}
